c82326e7b2846a9b5bb40f5dd6613db1718194a4
wood-pecker <mr.pianopost@mail.ru>
2024-07-22 14:57:28 +0300

    Kelvin to Celsius conversion

--- weather.py
+++ weather.py
@@ -7,6 +7,17 @@ API_KEY = ''
 from private_settings import API_KEY
 
 
+def kelvin_to_celsius(temp):
+    # Convert Kelvin to Celsius
+    return int(temp - 273.15)
+
+
 def get_weather(location):
     url = BASE_URL + 'q=' + location + '&appid=' + API_KEY
-    return requests.get(url).json()
+    response = requests.get(url).json()
+    
+    # Convert data 
+    response['main']['temp'] = kelvin_to_celsius(response['main']['temp'])     
+    response['main']['feels_like'] = kelvin_to_celsius(response['main']['feels_like'])
+    
+    return response
